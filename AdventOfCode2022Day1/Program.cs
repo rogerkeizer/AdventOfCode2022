ed2647a60f24c24c32371a1efe34ab4885eaf82d
Roger <roger@1bits.cloud>
2023-10-28 15:33:19 +0200

    Add project files.

---
+++ AdventOfCode2022Day1/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * https://adventofcode.com/2022/day/1
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> input = new();
+
+        List<int> output = new();
+
+        Dictionary<int, List<int>> cals = new();
+
+        var i = 1;
+
+        using StreamReader reader = new("input.txt");
+        while (reader != null && !reader.EndOfStream)
+        {
+            var r = reader.ReadLine();
+
+            if (r != null)
+            {
+                if (string.IsNullOrWhiteSpace(r))
+                {
+                    cals.Add(i, input);
+
+                    i++;
+
+                    input = new();
+                }
+                else
+                {
+                    try
+                    {
+                        input.Add(Convert.ToInt32(r));
+                    }
+                    catch 
+                    {
+                        Console.WriteLine($"Unable to parse '{r}'");
+                    } 
+                }
+            }
+        }
+
+        foreach (var c in cals)
+        {
+            var total = 0;
+
+            foreach (var cc in c.Value)
+            {
+                total += cc;
+            }
+
+            output.Add(total);
+        }
+
+        if (output.Count == 0)
+        {
+            Console.WriteLine("no data found");
+        }
+        else 
+        {
+            Console.WriteLine(output.OrderByDescending(s => s).First());
+        }
+    }
+}
