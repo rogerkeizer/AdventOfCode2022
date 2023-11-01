fcc5e2d414862d9f68630ad8dabbfdcfa8e2b8e2
Roger <roger@1bits.cloud>
2023-11-01 17:52:31 +0100

    day 1 in powershell

---
+++ AdventOfCode2022Day1/ps/day1.ps1
@@ -0,0 +1,43 @@
+﻿$inputList = New-Object System.Collections.Generic.List[string]
+
+$file = ".\input.txt";
+
+$fileExists = Test-Path($file);
+
+if ($fileExists -eq $True)
+{
+    $lines = Get-Content $file;
+
+    $temp = 0;
+
+    Foreach ($line in $lines)
+    {
+        if ([string]::IsNullOrEmpty($line))
+        {
+            $inputList.Add($temp);
+
+            $temp = 0;
+        }
+        else 
+        {
+            $temp += [int]$line; 
+        }
+    }
+
+    $orderedList = $inputList | Sort-Object {[int]$_} -Descending;
+
+    Write-Host ("Elf with most calories: {0}" -f $orderedList[0]);
+
+    $three = 0;
+
+    For($i = 0; $i -le 2; $i++)
+    {
+        $three += $orderedList[$i];
+    }
+
+    Write-Host ("Calories of top three Elves: {0}" -f $three);
+}
+else 
+{
+    Write-Host("No input found");
+}
