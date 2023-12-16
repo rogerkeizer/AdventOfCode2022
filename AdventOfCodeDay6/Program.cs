d1b17766f1d51719971d27e125b10c8afaec06d0
Roger <roger@1bits.cloud>
2023-12-16 20:29:57 +0100

    day 6 - star 1

---
+++ AdventOfCodeDay6/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * https://adventofcode.com/2022/day/6
+ * 
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string dataStream = File.ReadAllText("input.txt");
+
+        List<char> found = new();
+
+        var isMarker = false;
+
+        for (int i = 3; i < dataStream.Length; i++)
+        {
+            if (i >= 3)
+            {
+                var marker = dataStream.Skip(i - 3).Take(4).Select(x => x).ToList();
+
+                isMarker = marker.Distinct().Count() == marker.Count;
+            }
+
+            if (isMarker) 
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-[Advent Of Code 2022](https://adventofcode.com/2022)
+# Advent Of Code 2022
+
+Solutions for [Advent Of Code 2022](https://adventofcode.com/2022) in C#
