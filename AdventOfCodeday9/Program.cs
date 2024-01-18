0ecb8a0e1421e24c97582e368729a0c00d439352
Roger <roger@1bits.cloud>
2024-01-18 07:39:02 +0100

    day 9, in progress

--- AdventOfCodeday9/Program.cs
+++ AdventOfCodeday9/Program.cs
@@ -12,7 +12,14 @@ namespace AdventOfCodeDay9
     {
         public static void Main(string[] args)
         {
-            var moves = ParseInput("input.txt");
+            SolveStar1();
+
+            SolveStar2();
+        }
+
+        private static void SolveStar1()
+        {
+            var moves = ParseInput("input-test1.txt");
 
             Position head = new(0, 0);
 
@@ -26,10 +33,53 @@ namespace AdventOfCodeDay9
 
                 tail = tail.Follow(head, tail);
 
-                positions.Add(new Position(tail.x, tail.y));
+                positions.Add(tail);
             }
 
-            Console.WriteLine($"{positions.Count}");
+            Console.WriteLine($"solution start 1: {positions.Count}");
+        }
+
+        private static void SolveStar2()
+        {
+            var moves = ParseInput("input-test1.txt");
+
+            int lengthRope = 2; //10
+
+            HashSet<Position> positions = new();
+
+            List<Position> rope = new();
+
+            for (int i = 0; i < lengthRope; i++)
+            {
+                rope.Add(new Position(0, 0));
+            }
+
+            int last = lengthRope - 1;
+
+            foreach (var move in moves)
+            {
+                int i = 0;
+
+                while (i < lengthRope)
+                {
+                    if (i == last)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        rope[i] = rope[i].Move(move);
+
+                        rope[i + 1] = rope[i + 1].Follow(rope[i], rope[i + 1]);
+
+                        positions.Add(new Position(rope[i + 1].x, rope[i + 1].y));
+                    }
+
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"solution start 2: {positions.Count}");
         }
 
         private static List<Direction> ParseInput(string input)
